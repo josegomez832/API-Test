0e4ab8a2959bb167417c3e1674701ff1b013aefc
josegomez832 <gomez.jose853@gmail.com>
2015-08-03 17:21:58 -0500

    Instagram and Google maps API integrated

--- api.php
+++ api.php
@@ -4,26 +4,105 @@
   <meta charset="utf-8">
   <title>jQuery.getJSON demo</title>
   <style>
+  html, body,  { height: 100%; margin: 0; padding: 0;}
   img {
     height: 100px;
     float: left;
   }
+  #map-canvas{
+  	width:50%;
+  	height:400px;
+  }
   </style>
+  
 </head>
 <body>
-<div id="results">Stuff</div>
 <?php
 //http://davidwalsh.name/xbox-api
-$request = "https://www.kimonolabs.com/api/4g7bvve6?apikey=OOckE2QltIVVGg3RLMXx9fQque4MWaH6";
+$client_id = '9544d2a52af545ea911ea49e6c17e3a1';
+$client_secret = '4a95cc1c09664b16b0ed17d105144d85';
+$request = "https://api.instagram.com/v1/media/search?lat=29.817178&lng=-95.4012915&client_id=".$client_id;
 $response = file_get_contents($request);
 $results = json_decode($response, TRUE);
+echo '<div style="height:800px;overflow:scroll;">';
 echo "<pre>";
 print_r($results);
 echo "</pre>";
+echo '</div>';
 ?>
-<h1><?php echo $results['name']; ?></h1>
-<h1><a href="<?php echo $results['results']['collection1']['0']['article_title']['href']; ?>" target="_blank"><?php echo $results['results']['collection1']['0']['article_title']['text']; ?></a></h1>
-<p><?php echo $results['results']['collection1']['0']['excerpt']; ?></p>
+<div id="map-canvas"></div>
+<h1>Hashtag</h1>
+<ul>
+<?php foreach($results['data'] as $info){ ?>
+<li style="display:inline-block;width:150px;margin:40px;">
+	
+<p><a href="<?php echo $info['link']; ?>" target="_blank"><?php echo $info['caption']['text']; ?></a></p>
+<img src="<?php echo $info['images']['standard_resolution']['url']; ?>" /><p><?php echo $info['user']['username']; ?></p>
+	<p>
+	<?php foreach($info['tags'] as $tags){ ?>
+		<strong>#<?php echo $tags; ?></strong>, 
+	<?php } ?>
+	</p></li>
+<?php }?>
+</ul>
+</div>
 
+
+
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAKB7Oy4gwlex36Tm9Pq676FR6C8fwJq7k"></script>
+  <script type="text/javascript">
+      function initialize() {
+      	var instgram_location = new google.maps.LatLng(<?php echo $results['data'][0]['location']['latitude']; ?>,<?php echo $results['data'][0]['location']['longitude']; ?>);
+      	var styles = [
+			  {
+			    stylers: [
+			      { hue: "#00ffe6" },
+			      { saturation: -20 }
+			    ]
+			  },{
+			    featureType: "road",
+			    elementType: "geometry",
+			    stylers: [
+			      { lightness: 100 },
+			      { visibility: "simplified" }
+			    ]
+			  },{
+			    featureType: "road",
+			    elementType: "labels",
+			    stylers: [
+			      { visibility: "off" }
+			    ]
+			  }
+			];
+		var styledMap = new google.maps.StyledMapType(styles, {name: "Styled Map"});
+        var mapOptions = {
+          center: instgram_location,
+          zoom: 12,
+          mapTypeControlOptions: {
+     		 mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+    		}
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions );
+         	map.mapTypes.set('map_style', styledMap);
+  			map.setMapTypeId('map_style');
+        var marker = new google.maps.Marker({
+      		position:instgram_location,
+      		map:map,
+      		title: "<?php echo $results['data'][0]['location']['name'];?>"
+      	});
+      	var homeWindow = new google.maps.InfoWindow({
+      		content: "<?php echo $results['data'][0]['location']['name'];?>"
+      	});
+  		google.maps.event.addListener(marker, 'click', function() {
+      	homeWindow.open(map, marker);
+      	map.getBounds()
+  		console.log(map.getBounds());
+
+  		});
+
+      }
+      
+      google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,5 +1,203 @@
-<?php
-$url = "http://abduzeedo.com/";
-$output = file_get_contents($url);
-echo $output;
-?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7 not-logged-in "> <![endif]-->
+<!--[if IE 7]>         <html lang="en" class="no-js lt-ie9 lt-ie8 not-logged-in "> <![endif]-->
+<!--[if IE 8]>         <html lang="en" class="no-js lt-ie9 not-logged-in "> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js not-logged-in "> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <title>Page Not Found • Instagram</title>
+
+        <script type="text/javascript">
+  WebFontConfig = {
+    custom: {
+      families: ['proxima-nova:n3,n4,n6,n7'],
+      urls: ['//instagramstatic-a.akamaihd.net/bluebar/f88f1da/cache/styles/fonts.css']
+    }
+  };
+</script>
+<script src="//instagramstatic-a.akamaihd.net/bluebar/f88f1da/scripts/webfont.js" type="text/javascript" async></script>
+
+        
+    
+        <meta name="robots" content="noimageindex">
+        
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+
+        
+    <meta id="viewport" name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, minimum-scale=1, maximum-scale=1">
+
+
+        <script type="text/javascript">
+        (function() {
+            var docElement = document.documentElement;
+            var classRE = new RegExp('(^|\\s)no-js(\\s|$)');
+            var className = docElement.className;
+            docElement.className = className.replace(classRE, '$1js$2');
+        })();
+        </script>
+
+        <link href="https://api.instagram.com/publicapi/v1/users/josegomez832" rel="alternate" hreflang="x-default" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=el" rel="alternate" hreflang="el" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=gu" rel="alternate" hreflang="gu" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=en" rel="alternate" hreflang="en" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=af" rel="alternate" hreflang="af" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=vi" rel="alternate" hreflang="vi" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=it" rel="alternate" hreflang="it" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=kn" rel="alternate" hreflang="kn" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=pt-br" rel="alternate" hreflang="pt-br" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=cs" rel="alternate" hreflang="cs" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=id" rel="alternate" hreflang="id" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=es" rel="alternate" hreflang="es" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=ru" rel="alternate" hreflang="ru" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=nl" rel="alternate" hreflang="nl" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=pt" rel="alternate" hreflang="pt" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=zh-tw" rel="alternate" hreflang="zh-tw" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=nb" rel="alternate" hreflang="nb" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=tr" rel="alternate" hreflang="tr" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=ne" rel="alternate" hreflang="ne" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=zh-cn" rel="alternate" hreflang="zh-cn" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=tl" rel="alternate" hreflang="tl" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=pa" rel="alternate" hreflang="pa" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=th" rel="alternate" hreflang="th" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=te" rel="alternate" hreflang="te" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=pl" rel="alternate" hreflang="pl" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=ta" rel="alternate" hreflang="ta" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=fr" rel="alternate" hreflang="fr" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=hr" rel="alternate" hreflang="hr" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=bn" rel="alternate" hreflang="bn" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=de" rel="alternate" hreflang="de" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=da" rel="alternate" hreflang="da" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=hi" rel="alternate" hreflang="hi" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=fi" rel="alternate" hreflang="fi" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=hu" rel="alternate" hreflang="hu" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=ja" rel="alternate" hreflang="ja" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=ml" rel="alternate" hreflang="ml" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=ko" rel="alternate" hreflang="ko" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=sv" rel="alternate" hreflang="sv" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=ur" rel="alternate" hreflang="ur" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=sk" rel="alternate" hreflang="sk" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=si" rel="alternate" hreflang="si" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=ms" rel="alternate" hreflang="ms" />
+<link href="https://api.instagram.com/publicapi/v1/users/josegomez832?hl=mr" rel="alternate" hreflang="mr" />
+
+        
+    
+    
+        <link rel="icon" sizes="any" mask href="//instagramstatic-a.akamaihd.net/bluebar/f88f1da/images/ico/favicon.svg">
+        <meta name="theme-color" content="#125688">
+        <link rel="Shortcut Icon" type="image/x-icon" href="//instagramstatic-a.akamaihd.net/bluebar/f88f1da/images/ico/favicon.ico">
+    
+    
+        <link rel="apple-touch-icon-precomposed" href="//instagramstatic-a.akamaihd.net/bluebar/f88f1da/images/ico/apple-touch-icon-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="//instagramstatic-a.akamaihd.net/bluebar/f88f1da/images/ico/apple-touch-icon-72x72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="//instagramstatic-a.akamaihd.net/bluebar/f88f1da/images/ico/apple-touch-icon-114x114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="//instagramstatic-a.akamaihd.net/bluebar/f88f1da/images/ico/apple-touch-icon-144x144-precomposed.png">
+    
+    
+    <link href="//instagramstatic-a.akamaihd.net/bluebar/f88f1da/cache/styles/distillery/dialog-main.css" type="text/css" rel="stylesheet"></link>
+
+    
+    <script src="//instagramstatic-a.akamaihd.net/bluebar/f88f1da/scripts/jquery.js" type="text/javascript"></script>
+    <script src="//instagramstatic-a.akamaihd.net/bluebar/f88f1da/scripts/bluebar.js" type="text/javascript"></script>
+
+
+    </head>
+    <body class=" p-error dialog-404">
+        
+    
+        <div class="root">
+        
+            <div class="page">
+                
+                    
+                        <header class="top-bar top-bar-new">
+    <div class="top-bar-wrapper">
+        <div class="logo"><a href="/">Instagram</a></div>
+
+        <div class="top-bar-left">
+            <ul class="top-bar-actions">
+                <li>
+                    <a class="top-bar-home" href="/" label=Home><i></i></a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="top-bar-right account-state" id="top_bar_right">
+            <ul class="top-bar-actions">
+                
+                
+                <li id="link_profile" class="link-signin">
+                    <a href="/accounts/login/" class="loginLink">
+                        <i></i>
+                        <strong>Log in</strong>
+                    </a>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+</header> <!-- .top-bar -->
+                    
+                
+
+                
+                <div class="main">
+                    
+	<div class="error-container">
+	
+
+    <h2>Sorry, this page isn't available.</h2>
+
+    <p>
+        The link you followed may be broken, or the page may have been removed.
+        <a href="/">Go back to Instagram.</a>
+    </p>
+
+
+
+	</div>
+
+                </div> <!-- .main -->
+                
+
+            </div> <!-- .page -->
+
+            
+            <footer class="page-footer" role="contentinfo">
+                <div class="wrapper">
+                    <nav>
+                        <ul>
+                            <li><a href="/about/us/">About us</a></li>
+                            <li><a href="http://help.instagram.com/">Support</a></li>
+                            <li><a href="http://blog.instagram.com/">Blog</a></li>
+                            <li><a href="http://instagram.com/press/">Press</a></li>
+                            <li><a href="/developer/">API</a></li>
+                            <li><a href="/about/jobs/">Jobs</a></li>
+                            <li><a href="/legal/privacy/">Privacy</a></li>
+                            <li><a href="/legal/terms/">Terms</a></li>
+                        </ul>
+                    </nav>
+
+                    <p class="copyright">© 2015 Instagram</p>
+                </div>
+            </footer>
+            
+        
+        <div id="reactModalMountPoint"></div>
+    </div> <!-- .root -->
+    
+    
+
+        
+    <script src=//instagramstatic-a.akamaihd.net/bluebar/f88f1da/scripts/polyfills/es5-shim.min.js></script>
+<script src=//instagramstatic-a.akamaihd.net/bluebar/f88f1da/scripts/polyfills/es5-sham.min.js></script>
+<script type="text/javascript">window._sharedData = {"static_root":"\/\/instagramstatic-a.akamaihd.net\/bluebar\/f88f1da","entry_data":{},"hostname":"api.instagram.com","platform":{"is_touch":false,"app_platform":"web"},"qe":{"su":false},"display_properties_server_guess":{"viewport_width":360,"pixel_ratio":1.5},"country_code":"US","language_code":"en","gatekeepers":{"tr":false,"search":true,"top_posts":true},"config":{"dismiss_app_install_banner_until":null,"viewer":null,"csrf_token":"NOTPROVIDED"},"environment_switcher_visible_server_guess":true};</script>
+
+    
+
+    </body>
+</html>
